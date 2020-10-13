2000179b24de360322942eb4852f6eed3f63e848
Trenton Radford <Trenton.Radford@gmail.com>
2020-10-13 13:56:31 -0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+/*var welcome_message = "This is a welcome message stored inside a variable";
+var isPremUser = false;
+if (isPremUser == true) {
+    console.log("thanks for being a loyal customer");
+} else {
+	console.log("you should subscribe")
+}
+------------------------------------------------
+var myAge = 4;
+if (myAge < 1)
+    console.log("you are a baby");
+else if (myAge < 3)
+    console.log("you are a toddler");
+else if (myAge <= 18)
+    console.log("you are a teenager");
+else if (myAge < 40)
+    console.log("you are an adult");
+else 
+	console.log("you must be old");
+----------------------------------------------------
+
+for (i = 0; i < 10; i++) {
+    console.log("current i value " + i)
+	for (j = 0; j < 10; j++)
+    console.log("current j value" + j)
+}
+--------------------------------------------------
+
+function pName(name, age) {
+    console.log("Hello", name, "you are", age);
+}
+
+pName("nick", 28);
+----------------------------------------------
+
+function buttonClicked() {
+    console.log("button clicked")
+}*/
+
+Function myFun(){
+    console.log("my name is trent")
+}
+myFun();
